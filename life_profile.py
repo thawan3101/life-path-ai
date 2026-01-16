6e75f5f9bf3be47aafaf31443922f54503ba8ebd
thawan3101 <xxxvvvv857@gmail.com>
2026-01-16 17:20:37 +0700

    Add function to build a life profile dictionary

---
+++ life_profile.py
@@ -0,0 +1,8 @@
+def build_life_profile(money, time, place, problems, age_group):
+    return {
+        "money": money,
+        "time": time,
+        "place": place,
+        "problems": problems,
+        "age_group": age_group
+    }
